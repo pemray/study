82aed0157ac4792d9eda44ad38de70022c5a555e
PLL <864624271@qq.com>
2019-11-13 16:43:36 +0800

    ..

---
+++ app/src/main/java/com/zsj/pang/study/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zsj.pang.study;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
